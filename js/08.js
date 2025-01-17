fc4067066c576d354ce12ee7050682fc8774c9fd
Alejandro <arobles@uniempresarial.edu.co>
2025-01-17 14:41:12 -0500

    Concatenación y template strings

---
+++ js/08.js
@@ -0,0 +1,9 @@
+const nombre = 'Alejandro';
+const email = 'correo@correo.com';
+
+
+//Concatenacion
+console.log("Nombre cliente: " + nombre + " Email:  " + email);
+
+// Template strings - strings literals
+console.log(`Nombre cliente: ${nombre} Email: ${email}`);
